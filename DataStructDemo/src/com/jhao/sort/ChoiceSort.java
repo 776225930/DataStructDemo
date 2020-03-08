002274bf5ef772f0c517f164dd569c39cc2a1ba9
BugMaker <776225930@qq.com>
2020-03-08 19:50:40 +0800

    简单选择排序

---
+++ DataStructDemo/src/com/jhao/sort/ChoiceSort.java
@@ -0,0 +1,45 @@
+package com.jhao.sort;
+
+/**
+ * @author JiangHao
+ * @date 2020/3/8
+ * @describe
+ */
+public class ChoiceSort {
+
+
+    public static int[] sort(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i < array.length; i++) {
+            //存放最小数的下表
+            int minIndex = i;
+            for (int j = minIndex; j < array.length; j++) {
+                //找到最小的数
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {86, 11, 77, 23, 32, 45, 58, 63, 93, 4, 37, 22};
+        System.out.println(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("=========================================");
+        int[] sortedArray = sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(sortedArray[i] + " ");
+        }
+    }
+}
