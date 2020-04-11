aee2f7cd47e496135b960b93720f0e6e3ade5a95
BugMaker <776225930@qq.com>
2020-04-12 00:48:37 +0800

    wait()和notifyAll()用法

---
+++ ThreadDemo/src/com/jhao/threaddemo/TestWait.java
@@ -0,0 +1,9 @@
+package com.jhao.threaddemo;
+
+/**
+ * @author JiangHao
+ * @date 2020/4/11
+ * @describe
+ */
+public class TestWait {
+}
---
+++ ThreadDemo/src/com/jhao/threaddemo/waitandnotify/TestWN.java
@@ -0,0 +1,9 @@
+package com.jhao.threaddemo.waitandnotify;
+
+/**
+ * @author JiangHao
+ * @date 2020/4/12
+ * @describe
+ */
+public class TestWN {
+}
